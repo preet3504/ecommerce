2d861b4ef187577586a4dc4ddfd2e3c7cd41bed5
preet3504 <preetpatel3504@gmail.com>
2026-01-31 17:08:39 +0530

    Setup session provider

--- src/app/(user)/layout.jsx
+++ src/app/(user)/layout.jsx
@@ -1,14 +1,10 @@
-"use client"
-import { SessionProvider } from "next-auth/react"
 import Navbar from "@/components/layout/Navbar"
 
 export default function UserLayout({ children }) {
   return (
-    <SessionProvider>
-      <div className="min-h-screen bg-white">
-        <Navbar />
-        <main>{children}</main>
-      </div>
-    </SessionProvider>
+    <div className="min-h-screen bg-white">
+      <Navbar />
+      <main>{children}</main>
+    </div>
   )
 }
